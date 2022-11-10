0559a299e176aa6b58b2093a7b7d0059ce6a94cf
FTrain27 <thewizard2742@protonmail.com>
2022-11-09 23:20:13 -0500

    Slowed printing down some so you can sing along

--- My_bottles.py
+++ My_bottles.py
@@ -3,29 +3,34 @@
 from time import sleep
 
 bstart=""
-while True:
+def setup(bstart):
     try:
-        bstart=int(input("How many bottles of beer do you want to start with? Enter a number 1-99\n"))
+        bstart=int(input("How many bottles of beer do you want to start with? Enter a number 1-99 or just press enter to start from 99\n"))
         if bstart>99:
             print("That's too many bottles of beer!")
-            continue
+            setup(bstart)
         if bstart<=0:
             print("That's not enough bottles of beer!")
-            continue
+            setup(bstart)
+        if bstart=="":
+            print ("Starting from 99..")
+            sleep(2)
+            bstart=99
+            beer(bstart)
     except ValueError:
         if bstart=="":
             print ("Starting from 99..")
             sleep(2)
             bstart=99
-            break
+            beer(bstart)
         else:
             print("Value Error: non numeric data")
-            continue
+            setup(bstart)
     except Exception:
             print("Unknown Exception: ")
-            continue
+            setup(bstart)
     if bstart!=None:
-        break        
+        beer(bstart)        
 
 def beer(bstart):
     while bstart>0:
@@ -34,17 +39,17 @@ def beer(bstart):
         else:
             word="bottle"
         print("{0} {1} of beer on the wall!\n{0} {1} of beer!\nTake one down and pass it around".format(bstart,word))
-        sleep(1)
+        sleep(2.5)
         bstart-=1
         if bstart>1:
             print("{} {} of beer on the wall!\n".format(bstart,word))
-            sleep(1)
+            sleep(2.5)
         elif bstart==1:
             print("{} {} of beer on the wall!\n".format(bstart,"bottle"))
-            sleep(1)
+            sleep(2.5)
         else:
             print("No more bottles of beer on the wall!\n")
             print ("Thanks for playing with me!")
 
 if __name__ == "__main__":
-    beer(bstart)
+    setup(bstart)
